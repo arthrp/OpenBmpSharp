1f42dea19f5dbd3e96244b37322afeed0275ce80
Arthur <arthrp@users.noreply.github.com>
2023-11-22 01:39:23 +0200

    Add test bmp file

--- OpenBmpSharp.Test/BmpParserTests.cs
+++ OpenBmpSharp.Test/BmpParserTests.cs
@@ -1,3 +1,5 @@
+using OpenBmpSharp.Pds;
+
 namespace OpenBmpSharp.Test;
 
 public class BmpParserTests
@@ -9,5 +11,19 @@ public class BmpParserTests
         var fileStream = new MemoryStream(new byte[] {0xFF, 0xD8});
 
         Assert.Throws<ArgumentException>(() => bmpParser.Parse(fileStream));
+    }
+
+    [Test]
+    public void ParsingBitmapFile_ProducesRealData()
+    {
+        var bmpParser = new BmpParser();
+        var b = bmpParser.Parse("test.bmp");
+        
+        Assert.That(b.Size, Is.EqualTo(154));
+        Assert.That(b.Height, Is.EqualTo(1));
+        Assert.That(b.Width, Is.EqualTo(4));
+        Assert.That(b.ColorPlanes, Is.EqualTo(1));
+        Assert.That(b.BitsPerPixel, Is.EqualTo((short)32));
+        Assert.That(b.CompressionMethod, Is.EqualTo(CompressionMethod.BI_BITFIELDS));
     }
 }
